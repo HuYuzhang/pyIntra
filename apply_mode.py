973ce9db34de06f4283cf148db7ab0d7941bc979
msi_vm <yuzhanghu@pku.edu.cn>
2018-11-17 19:29:13 -0800

    test 3 mode

--- apply_mode.py
+++ apply_mode.py
@@ -14,7 +14,7 @@ img_path = 'img/'
 # target_path = 'target/'
 gt_path = 'gt/'
 pred_path = 'pred/'
-h5_path = '/home/hyz/angle_test.h5'
+h5_path = '/home/hyz/planar_test.h5'
 
 # initilization
 deploy_path = root_path + deploy_path
@@ -45,7 +45,7 @@ anglenet = caffe.Net(deploy_path, anglemodel_path, caffe.TEST)
 # 1-sampe refer to one picture
 # We store the gt picture in traget_path, and predicted picture in pred_path
 st_id = 0
-ed_id = 100
+ed_id = 1000
 sample_number = ed_id - st_id
 stride = 1
 handler = h5Handler(h5_path)
@@ -65,7 +65,7 @@ for i in range(sample_number):
     img[32:64, 32:64] = label.reshape([32, 32], order='F') * 255.0
     # write img to the gt directory
     # for normal data, we consider nothing
-    # print(i)
+    print(i)
     if np.var(img) > 200:
         # cv2.imwrite(gt_path + str(i) + '.png', img)
         label = label.reshape([32, 32], order='F') * 255.0
@@ -93,6 +93,49 @@ for i in range(sample_number):
         # img[32:, 32:] = vec[...]
         # write pred_img to the pred directory
         # cv2.imwrite(pred_path + str(i) + '.png', img)
+print(np.mean(psnrs[0]), np.mean(psnrs[1]), np.mean(psnrs[2]))
+print(np.mean(ssims[0]), np.mean(ssims[1]), np.mean(ssims[2]))
+
+# now test without filter
+psnrs = [[], [], []]
+ssims = [[], [], []]
+for i in range(sample_number):
+    print(i)
+    data = datas[i:i+1, :, :, :]
+    label = labels[i:i+1, :, :, :]
+    img[:32, :] = data[:, :2048, :, :].reshape([32, 64], order='F') * 255.0
+    img[32:64, :32] = data[:, 2048:, :, :].reshape([32, 32], order='F') * 255.0
+    img[32:64, 32:64] = label.reshape([32, 32], order='F') * 255.0
+    # write img to the gt directory
+    # for normal data, we consider nothing
+    # print(i)
+    # if np.var(img) > 200:
+        # cv2.imwrite(gt_path + str(i) + '.png', img)
+    label = label.reshape([32, 32], order='F') * 255.0
+    dcnet.blobs['data'].data[...] = data
+    planarnet.blobs['data'].data[...] = data
+    anglenet.blobs['data'].data[...] = data
+    # test for dcnet
+    vec = dcnet.forward()
+    vec = vec['efc4'].reshape([32, 32], order='F') * 255.0
+    tmp_stat = test_quality(label, vec)
+    psnrs[0].append(tmp_stat['psnr'])
+    ssims[0].append(tmp_stat['ssim'])
+    # test for planarnet
+    vec = planarnet.forward()
+    vec = vec['efc4'].reshape([32, 32], order='F') * 255.0
+    tmp_stat = test_quality(label, vec)
+    psnrs[1].append(tmp_stat['psnr'])
+    ssims[1].append(tmp_stat['ssim'])
+    # test for anglenet
+    vec = anglenet.forward()
+    vec = vec['efc4'].reshape([32, 32], order='F') * 255.0
+    tmp_stat = test_quality(label, vec)
+    psnrs[2].append(tmp_stat['psnr'])
+    ssims[2].append(tmp_stat['ssim'])
+    # img[32:, 32:] = vec[...]
+    # write pred_img to the pred directory
+    # cv2.imwrite(pred_path + str(i) + '.png', img)
 print(np.mean(psnrs[0]), np.mean(psnrs[1]), np.mean(psnrs[2]))
 print(np.mean(ssims[0]), np.mean(ssims[1]), np.mean(ssims[2]))
     
