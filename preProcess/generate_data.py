ee2ba98ebc7faa0dff1b92529bc154a17214bf9b
xps_vm <yuzhanghu@pku.edu.cn>
2018-11-26 06:07:09 -0800

    generate data_set with 3 mode

--- preProcess/generate_data.py
+++ preProcess/generate_data.py
@@ -3,9 +3,14 @@ import cv2 as cv
 from h5 import h5Handler
 
 dump_path = 'dump_dir.txt'
-dec_path = '../raw_data/dec.yuv'
-gt_path = '../raw_data/video.yuv'
+dec_path = '../../raw_data/dec.yuv'
+gt_path = '../../raw_data/video.yuv'
 target_path = '../img/'
+
+planar_h5_path = ''
+dc_h5_path = ''
+angle_h5_path = ''
+
 height = 1024
 width = 1792
 block_size = 32
@@ -40,13 +45,23 @@ dcvars = []
 planarvars = []
 anglevars = []
 
-# --------------for debug------------------
-cnt = 0
-datas = np.zeros([20, 3072, 1, 1])
-labels = np.zeros([20, 1024, 1, 1])
-flag = True   
-handler = h5Handler('/home/hyz/lab/intra/train/train.h5')     
-# --------------for debug------------------
+planarinput = np.zeros([1500, 3072, 1, 1])
+planarlabel = np.zeros([1500, 1024, 1, 1])
+dcinput =np.zeros([1500, 3072, 1, 1])
+dclabel = np.zeros([1500, 1024, 1, 1])
+angleinput = np.zeros([1500, 3072, 1, 1])
+anglelabel = np.zeros([1500, 1024, 1, 1])
+
+planar_handler = h5Handler(planar_h5_path)
+dc_handler = h5Handler(dc_h5_path)
+angle_handler = h5Handler(angle_h5_path)
+# # --------------for debug------------------
+# cnt = 0
+# datas = np.zeros([20, 3072, 1, 1])
+# labels = np.zeros([20, 1024, 1, 1])
+# flag = True   
+# handler = h5Handler('/home/hyz/lab/intra/train/train.h5')     
+# # --------------for debug------------------
 
 with open(dump_path) as f:
     while True:
@@ -64,6 +79,10 @@ with open(dump_path) as f:
         # --------------for debug------------------
 
         if y == 0 and x == 0:
+            pc = 0
+            dc = 0
+            ac = 0
+
             gt_img = read_frame(gt_path, f_id, height, width)
             dec_img = read_frame(dec_path, f_id, height, width)
             print(f_id)
@@ -94,16 +113,42 @@ with open(dump_path) as f:
         # --------------for debug------------------(write data to hdf5 file)
 
         if mode == planar_mode:
+            planarinput[pc, :, :, :] = input[0,:,:,:]
+            planarlabel[pc, :, :, :] = label[0,:,:,:]
+            pc = pc + 1
             planarvars.append(np.var(test_img))
         elif mode == dc_mode:
+            dcinput[dc, :, :, :] = input[0,:,:,:]
+            dclabel[dc, :, :, :] = label[0,:,:,:]
+            dc = dc + 1
             dcvars.append(np.var(test_img))
         else:
+            angleinput[ac, :, :, :] = input[0,:,:,:]
+            anglelabel[ac, :, :, :] = label[0,:,:,:]
+            ac = ac + 1
             anglevars.append(np.var(test_img))
         # cv.imwrite(target_path + str(y) + '_' + str(x) + '.png', test_img)
 
+        # Then check if we have arrive the final block of this frame
+        if y == 1760 and x == 992:
+            # now begin to write data to the h5 file
+            if f_id == 0:
+                # create mode
+                planar_handler.write(planarinput(:pc,:,:,:), planarlabel(:pc,:,:,:), create=True)
+                dc_handler.write(dcinput(:dc,:,:,:), dclabel(:dc,:,:,:), create=True)
+                angle_handler.write(angleinput(:ac,:,:,:), anglelabel(:ac,:,:,:), create=True)
+            else:
+                # append mode
+                planar_handler.write(planarinput(:pc,:,:,:), planarlabel(:pc,:,:,:), create=False)
+                dc_handler.write(dcinput(:dc,:,:,:), dclabel(:dc,:,:,:), create=False)
+                angle_handler.write(angleinput(:ac,:,:,:), anglelabel(:ac,:,:,:), create=False)
+            pc = 0
+            dc = 0
+            ac = 0
 
-print(np.mean(planarvars))
-print(np.mean(dcvars))
-print(np.mean(anglevars))
+
+print(np.mean(planarvars), len(planarvars))
+print(np.mean(dcvars), len(dcvars))
+print(np.mean(anglevars), len(anglevars))
 
 
